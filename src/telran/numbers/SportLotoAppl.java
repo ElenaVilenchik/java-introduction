a825058cbd09a0c4e709856483f6884fdf2d5134
User <User@192.168.0.132>
2022-06-21 13:49:57 +0300

    CW

---
+++ src/telran/numbers/SportLotoAppl.java
@@ -0,0 +1,20 @@
+package telran.numbers;
+
+/**
+ * 
+ * @author User
+ *application should print out 7 random nuvbers in the range [1-49]
+ */
+public class SportLotoAppl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//random number in the range [min - max]
+private static int getRandomNumber(int min, int max) {
+	return (int) (min + Math.random() * (max - min +1));
+	// (int)???
+}
+}
