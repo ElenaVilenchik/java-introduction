67bf1022f1503ed164c575456ffc26252ed00045
User <User@User-PC.Dlink>
2022-07-01 08:57:42 +0300

    A-03 Anagram updated 2.0

--- src/telran/numbers/Anagram2.java
+++ src/telran/numbers/Anagram2.java
@@ -9,30 +9,33 @@ public class Anagram2 {
 				 return false; 
 				
 			 //if the string contains more than just letters
-			 for(int i=0;i<sourceWord.length();i++){
+			 for(int i=0;i<targetWord.length();i++){
 				 if(! Character.isLetter(sourceWord.charAt(i)) 
 						 || ! Character.isLetter(targetWord.charAt(i))) {
 			    	return false;
 				 }
 			 }
-		    int[] strCountTargetWord=getASCIICountArr(sourceWord + targetWord);
+		    int[] strCountTargetWord=getASCIICountArr(sourceWord, targetWord);
 		    
 		    for(int i=0;i<256;i++){
-		        if(strCountTargetWord[i] % 2 != 0) {
+		        if(strCountTargetWord[i] != 0) {
 		            return false;
 		        }
 		    }
 		    return true;
 		}
 
-		public static int[] getASCIICountArr(String str){
+		public static int[] getASCIICountArr(String str1, String str2){
 		    char c;
 		    //Array size 256 for ASCII
 		    int[] strCountArr=new int[256];
-		    for(int i=0;i<str.length();i++){
-		        c=str.charAt(i); 
+		    for(int i=0;i<str1.length();i++){
+		        c=str1.charAt(i); 
 		        c=Character.toUpperCase(c);
 		        strCountArr[(int)c]++;
+		        c=str2.charAt(i); 
+		        c=Character.toUpperCase(c);
+		        strCountArr[(int)c]--;
 		    	}
 		    return strCountArr;
 		}
--- src/telran/numbers/tests/Anagram2Test.java
+++ src/telran/numbers/tests/Anagram2Test.java
@@ -5,18 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import telran.numbers.Anagram2;
+import telran.text.Strings;
 
 class Anagram2Test {
 		@Test
 		void testIsAnagram() {
 			assertTrue(Anagram2.isAnagram("anagramm", "nagarmam"));
 			assertTrue(Anagram2.isAnagram("aaanagramm", "nagarmmaaa"));
-			assertTrue(Anagram2.isAnagram("AnagraMm", "grmmNaaa"));
+			assertFalse(Anagram2.isAnagram("AnagraMm", "grNnnaaa"));
 			assertFalse(Anagram2.isAnagram("anagramm", "nagaraZm"));
 			assertFalse(Anagram2.isAnagram("anag", "nagaramm"));
-			assertFalse(Anagram2.isAnagram("a", ""));
-			assertFalse(Anagram2.isAnagram("a", "sss"));
+			assertFalse(Anagram2.isAnagram("a", "!"));
+			assertFalse(Anagram2.isAnagram("!", "s"));
 			assertFalse(Anagram2.isAnagram("an121agramm", "an211agramm"));
 			assertFalse(Anagram2.isAnagram("!anagramm", "anagram"));
+		}
+		void isAnagramTests() {
+			String str1 = "Hello";
+			String str2 = "Helll";
+			String str3 = "eloHl";
+			String str4 = "Hel";
+			assertTrue(Strings.isAnagram(str1, str3));
+			assertFalse(Strings.isAnagram(str1, str2));
+			assertFalse(Strings.isAnagram(str1, str4));
+			assertTrue(Strings.isAnagram(str1, str1));
 		}
 	}
