1b19ee8553332f88fa588b60178efea6a121c871
User <User@User-PC.Dlink>
2022-07-01 20:17:13 +0300

    class Work

---
+++ src/telran/text/Strings.java
@@ -0,0 +1,18 @@
+package telran.text;
+
+public class Strings {
+	/**
+	 * 
+	 * @param str1 ASCII string with no repeated symbols
+	 * @param str2 ASCII string with no repeated symbols
+	 * @return array with two numbers
+	 * first - number of the symbols of the str2 that exist in str1 at the indexes
+	 * second - number of the symbols of the str2 that exist in str1 at the different
+	 */
+public static int[] deepNoRepeatedCompare(String str1, String str2) {
+		//1. additional helper array such that array['a'] is an index of the symbal 'a'...
+	return null;
+}
+
+
+}
