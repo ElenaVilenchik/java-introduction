a7a2f2d79f4955f1ba1dae62813c131e9ae0affc
User <User@192.168.0.132>
2022-07-03 19:13:36 +0300

    HW- 06

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -9,10 +9,29 @@ public class Strings {
 	 * first - number of the symbols of the str2 that exist in str1 at the indexes
 	 * second - number of the symbols of the str2 that exist in str1 at the different
 	 */
-public static int[] deepNoRepeatedCompare(String str1, String str2) {
-		//1. additional helper array such that array['a'] is an index of the symbal 'a'...
-	return null;
+	
+	public static int[] deepNoRepeatedCompare(String str1, String str2) {
+		int[] strCountArr=new int[2];
+		int 	numberIdenticalIndexes = 0, 
+				numberDifferentIndexes = 0;
+		
+		if (str1.equals(str2)) {
+			numberIdenticalIndexes = str1.length();
+		} else {
+		
+		for (int i = 0; i<str1.length(); i++) {
+			for (int j = 0; j<str2.length(); j++){
+				if((str2.charAt(j) == str1.charAt(i))) {
+					if (str1.indexOf(str1.charAt(i)) == str2.indexOf(str2.charAt(j)))
+						numberIdenticalIndexes++;
+						else numberDifferentIndexes++;	
+			}
+		}
+	}}
+		
+		strCountArr[0] = numberIdenticalIndexes;
+		strCountArr[1] = numberDifferentIndexes;
+		return strCountArr; 
+	}
 }
 
-
-}
--- src/telran/text/tests/StringsClassTests.java
+++ src/telran/text/tests/StringsClassTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import telran.numbers.Anagram2;
 import telran.text.Strings;
 
 class StringsClassTests {
@@ -16,14 +15,20 @@ class StringsClassTests {
 		String str3 = "19";
 		String str4 = "kugy";
 		String str5 = "98Tt,321";
+		String str6 = "";
+		String str7 = "3,tT";
 		int expected1[] = {8, 0};
 		int expected2[] = {1, 1};
 		int expected3[] = {0, 8};
 		int expected4[] = {0, 0};
+		int expected5[] = {0, 4};
 		assertArrayEquals(expected1, Strings.deepNoRepeatedCompare(str1, str2));
 		assertArrayEquals(expected2, Strings.deepNoRepeatedCompare(str1, str3));
 		assertArrayEquals(expected3, Strings.deepNoRepeatedCompare(str1, str5));
 		assertArrayEquals(expected4, Strings.deepNoRepeatedCompare(str1, str4));
+		assertArrayEquals(expected4, Strings.deepNoRepeatedCompare(str1, str6));
+		assertArrayEquals(expected5, Strings.deepNoRepeatedCompare(str1, str7));
+		assertArrayEquals(expected2, Strings.deepNoRepeatedCompare(str3, str1));
 		}
 	
 	}
